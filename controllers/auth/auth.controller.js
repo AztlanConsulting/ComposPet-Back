e940bce60c8bebdd154cfe4266348a9fb1a9c421
Kamila <kamiii.martineeez@gmail.com>
2026-04-15 10:41:46 -0600

    [ELIMINAR] codigo comentado

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -107,13 +107,6 @@ const login = async(req, res) => {
 
         res.cookie('refreshToken', refreshToken, cookieOptions);
 
-        // res.cookie('refreshToken', refreshToken, {
-        //     httpOnly: true,    
-        //     secure: process.env.NODE_ENV === 'production', 
-        //     sameSite: 'strict',
-        //     maxAge: 7 * 24 * 60 * 60 * 1000 
-        // });
-
         return res.status(200).json({
             id_usuario: user.id_usuario,
             correo: user.correo,
