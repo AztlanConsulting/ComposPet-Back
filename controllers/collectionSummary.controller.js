d7aa2b2df1e5e0f6f62c488a455dcddc8f07170f
JuanManuelMurilloLopez <142356988+JuanManuelMurilloLopez@users.noreply.github.com>
2026-04-16 13:23:11 -0600

    [Agregar] Rutas y estructura del controlador

---
+++ controllers/collectionSummary.controller.js
@@ -0,0 +1,18 @@
+
+const getSummary = async (req, res) => {
+
+}
+
+const deleteProduct = async (req, res) => {
+
+}
+
+const updateCollectionTotal = async(req, res) => {
+
+}
+
+module.exports = {
+    getSummary,
+    deleteProduct,
+    updateCollectionTotal,
+}
--- routes/collectionRequest.routes.js
+++ routes/collectionRequest.routes.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const collectionRequestController = require('../controllers/collectionRequest.controller');
+const collectionSummaryController = require('../controllers/collectionSummary.controller');
 
 
 router.post('/form02/obtener', collectionRequestController.getCurrentCollectionRequest);
 router.post('/form02/guardar', collectionRequestController.saveCollectionRequestFirstSection);
+router.get('/collection-summary', collectionSummaryController.getSummary);
+router.delete('/collection-summary/product/:idProduct', collectionSummaryController.deleteProduct);
+router.put('/collection/payment', collectionSummaryController.updateCollectionTotal);
 
 module.exports = router;
