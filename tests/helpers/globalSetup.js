e68df5cff73926dec6d5340fd86bbf708f909cca
Yessica <a01273574@tec.mx>
2026-04-25 14:22:49 -0600

    [MODIFICAR] Configuración de test en helper y github para migrations

--- tests/helpers/globalSetup.js
+++ tests/helpers/globalSetup.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: '.env.test' });
 module.exports = async () => {
     console.log('Corriendo migraciones en base de datos de pruebas...');
 
-    execSync('npx prisma migrate deploy', {
+    execSync('npx dotenv -e .env.test -- npx prisma db push --force-reset --accept-data-loss', {
         stdio: 'inherit',
         env: {
             ...process.env,
