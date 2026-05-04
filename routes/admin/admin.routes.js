4b180ace75e46848a28f847ab69ca7fe47cff86d
JuanManuelMurilloLopez <142356988+JuanManuelMurilloLopez@users.noreply.github.com>
2026-05-03 18:13:47 -0600

    [AGREGAR] Rutas para actualizar la información de un cliente

--- routes/admin/admin.routes.js
+++ routes/admin/admin.routes.js
@@ -9,4 +9,10 @@ router.get('/registrar-cliente', createNewClientController.getRegisterClient);
 // Ruta para registrar nuevo cliente
 router.post('/registrar-cliente', createNewClientController.postRegisterClient);
 
+// Ruta para obtener la información necesaria para aactualizar un cliente
+router.get('/actualizar-cliente');
+
+// Ruta para modificar la información de un cliente
+router.post('/actualizar-cliente')
+
 module.exports = router;
