9016da0511cd102c7ae30caed3ad74cb5e5e912f
JuanManuelMurilloLopez <142356988+JuanManuelMurilloLopez@users.noreply.github.com>
2026-05-07 14:50:52 -0600

    [CORREGIR] Permisos en rutas de admin

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const createNewClientController = require('../../controllers/admin/createNewClient.controller')
-const clientController = require('../../controllers/client.controller');
+const createNewClientController = require('../controllers/admin/createNewClient.controller')
+const clientController = require('../controllers/client.controller');
+const { requireRole } = require('../middlewares/roleAccess');
 
 // Ruta para obtener información para registrar nuevo cliente
-router.get('/registrar-cliente', createNewClientController.getRegisterClient);
+router.get('/registrar-cliente', requireRole("Administrador"), createNewClientController.getRegisterClient);
 
 // Ruta para registrar nuevo cliente
-router.post('/registrar-cliente', createNewClientController.postRegisterClient);
+router.post('/registrar-cliente', requireRole("Administrador"), createNewClientController.postRegisterClient);
 
 // Ruta para obtener la información necesaria para aactualizar un cliente
-router.get('/actualizar-cliente', clientController.getRoutes);
+router.get('/actualizar-cliente', requireRole("Administrador"), clientController.getRoutes);
 
 // Ruta para modificar la información de un cliente
-router.post('/actualizar-cliente', clientController.updateClient);
+router.post('/actualizar-cliente', requireRole("Administrador"), clientController.updateClient);
 
 module.exports = router;
