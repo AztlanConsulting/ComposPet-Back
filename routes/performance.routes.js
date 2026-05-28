2f43245c81cc9684a6e49e90b76249c9faf33c61
Yessica <a01273574@tec.mx>
2026-05-28 14:25:56 -0600

    [CORREGIR] Comentario

--- routes/performance.routes.js
+++ routes/performance.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const performanceController = require('../controllers/performance.controller');
 const { requireRole } = require('../middlewares/roleAccess');
 
-// Ruta para consultar el saldo del cliente
+// Ruta para manejar las métricas de Web Vitals de performance
 router.post('/metricas', performanceController.getWebVitalMetric);
 
 module.exports = router;
