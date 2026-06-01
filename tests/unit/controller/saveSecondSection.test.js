631a54f3ee591259408fb413e862e8d189f88cf1
Ale Arredondo <alejandrarredondo09@gmail.com>
2026-06-01 17:21:45 -0600

    [MODIFICAR] test unitario del controller

--- tests/unit/controller/saveSecondSection.test.js
+++ tests/unit/controller/saveSecondSection.test.js
@@ -49,7 +49,7 @@ describe('Controller - saveSecondSection', () => {
     expect(res.status).toHaveBeenCalledWith(400);
     expect(res.json).toHaveBeenCalledWith({
       success: false,
-      message: 'Cada producto debe incluir id_producto y cantidad.',
+      message: 'Faltan datos requeridos para guardar la segunda sección de la solicitud de recolección.',
     });
   });
 
