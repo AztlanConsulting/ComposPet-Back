4800d0a5f3c1e8ff3d0f18a74467601d6866a087
JuanManuelMurilloLopez <a01712218@tec.mx>
2026-06-08 11:13:15 -0600

    [MODIFICAR] Formato de horario para aceptar formatos H:MM

--- utils/editValidations.js
+++ utils/editValidations.js
@@ -18,7 +18,7 @@ const isValidAddress = (value) =>
     /^(?=.*[A-Za-zÀ-ÿ])[A-Za-zÀ-ÿ0-9.,#\-\s]{5,150}$/.test(value);
 
 const isValidSchedule = (value) =>
-    /^(0[1-9]|1[0-2]):[0-5]\d$/.test(value);
+    /^(0?[1-9]|1[0-2]):[0-5]\d$/.test(value);
 
 const normalizeText = (value = '') =>
     String(value).trim();
@@ -181,6 +181,7 @@ const validateRequestUpdate = (data) => {
     }
 
     if (schedule) {
+        console.log("HORARAIO:", schedule);
         if (containsEmoji(schedule)) {
             errors.schedule = 'El horario no puede contener emojis.';
         } else if (!isValidSchedule(schedule)) {
